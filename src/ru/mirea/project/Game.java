5fe666c27df11f76ad6ec7e90fb7172e5a866490
ankkarp <anna.a.k.2002@gmail.com>
2021-12-09 23:44:32 +0300

    tasks 19, 23 and 24

--- src/ru/mirea/project/Game.java
+++ src/ru/mirea/project/Game.java
@@ -2,27 +2,66 @@ package ru.mirea.project;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Game extends JPanel {
+public class Game extends JPanel implements KeyListener{
     int hp, x, y;
+    Image player = new ImageIcon("src/ru/mirea/project/player.png").getImage();
+    boolean isRunning = false;
 
     Game(){
-        repaint();
+        setFocusable(true);
+        requestFocusInWindow();
+    }
+
+    public void begin(){
+        isRunning = true;
+        x = 150;
+        y = 150;
     }
 
     public void paintComponent(Graphics g){
+        System.out.println(x + ", " + y);
+        super.paintComponent(g);
         hp = 3;
-        x = 250;
-        y = 250;
-        while (hp != 0) {
-            g.setColor(new Color(153, 13, 13));
-            g.fillRect(x, y, 50, 50);;
-        }
+        g.drawImage(player ,x, y,this);
+        setBackground(new Color(100, 100, 100));
     }
 
+    public static void main(String[] args) {
+        new Game();
+    }
 
     @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println(e.getKeyCode());
+        if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            x -= 10;
+        }
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            x += 10;
+        }
+        if(e.getKeyCode() == KeyEvent.VK_UP){
+            y -= 10;
+        }
+        if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            y += 10;
+        }
+        repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
     public Dimension getPreferredSize() {
-        return new Dimension(500, 500);
+        return new Dimension(800, 600);
     }
 }
